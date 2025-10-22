5a960799c2151909652f948f64345041485e9720
Aghnaya <nay.kenara@gmail.com>
2025-10-22 22:02:54 +0700

    update Events modul and global.css

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.show_events, name='event_list'),
     path('add/', views.add_event, name='add_event'),
+    path('json/', views.show_json, name='show_json'),
+    path('json/<str:filter_type>/', views.show_json_filtered, name='show_json_filtered'),
 ]
