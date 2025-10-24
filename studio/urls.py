e42d04985e9515cfdf21bc51a1657b56289de854
Faza <muhammad.faza44@ui.ac.id>
2025-10-24 11:29:23 +0700

    Added AJAX feature to Studio

--- studio/urls.py
+++ studio/urls.py
@@ -5,7 +5,7 @@ app_name = 'studio'
 urlpatterns = [
     path('', show_studio, name='show_studio'),
     path('add/', add_studio, name='add_studio'),
-    path('json/', show_json, name='json_all'),
-    path('json/<int:id>/', show_json_by_id, name='json_by_id'),
+    path('json/', show_json, name='show_json'),
+    path('delete/<uuid:id>/', delete_studio, name='delete_studio'),
 ]
 
